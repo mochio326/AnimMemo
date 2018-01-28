ce4f84ae48f095b6d7c04e456bc6ba6fb99608df
MOCHIO <326mochio@gmail.com>
2018-01-28 22:27:49 +0900

    エディット機能を追加

--- Contents/scripts/animmemo/edit.py
+++ Contents/scripts/animmemo/edit.py
@@ -28,13 +28,6 @@ class MemoDialogBase(QtWidgets.QDialog):
         self.btn_color.clicked.connect(self._select_color)
 
     def _edit_layout(self):
-        # ダイアログのOK/キャンセルボタンを用意
-        btns = QtWidgets.QDialogButtonBox(
-            QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel,
-            QtCore.Qt.Horizontal, self)
-        btns.accepted.connect(self.accept)
-        btns.rejected.connect(self.reject)
-
         vb = QtWidgets.QVBoxLayout()
 
         hb = QtWidgets.QHBoxLayout()
@@ -49,7 +42,6 @@ class MemoDialogBase(QtWidgets.QDialog):
         vb.addLayout(hb)
         self._set_button_color()
 
-        vb.addWidget(btns)
         return vb
 
     def _select_color(self):
@@ -83,7 +75,16 @@ class NewMemoDialog(MemoDialogBase):
         self.show()
 
     def _layout(self):
-        self.setLayout(self._edit_layout())
+        # ダイアログのOK/キャンセルボタンを用意
+        btns = QtWidgets.QDialogButtonBox(
+            QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel,
+            QtCore.Qt.Horizontal, self)
+        btns.accepted.connect(self.accept)
+        btns.rejected.connect(self.reject)
+        vb = self._edit_layout()
+        vb.addWidget(btns)
+
+        self.setLayout(vb)
 
     @staticmethod
     def gui(parent=None, fr=None):
@@ -106,49 +107,86 @@ class EditMemoDialog(MemoDialogBase):
             self.draw_data = draw_data
         self._add_widgets()
         self._layout()
+        self.memo_list_view.setCurrentIndex(self.memo_list_model.index(0, 0))
         self.setWindowTitle('Edit Memo')
         self.show()
 
     def _add_widgets(self):
         super(EditMemoDialog, self)._add_widgets()
-        self.view = QtWidgets.QTreeView()
-        self.view.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
-        self.view.setAlternatingRowColors(True)
-        self.model = QtGui.QStandardItemModel()
-        self.model.setHorizontalHeaderLabels(['fr', 'bg_color', 'comment'])
-        self.view.setModel(self.model)
+        self.memo_list_view = QtWidgets.QTreeView()
+        self.memo_list_view.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.memo_list_view.setAlternatingRowColors(True)
+        self.memo_list_model = QtGui.QStandardItemModel()
+        self.memo_list_model.setHorizontalHeaderLabels(['frame', 'color', 'comment'])
+        self.memo_list_view.setModel(self.memo_list_model)
+
+        memo_list_sel_model = self.memo_list_view.selectionModel()
+        memo_list_sel_model.selectionChanged.connect(self._memo_list_changed)
 
     def _layout(self):
         vb = self._edit_layout()
 
         hb0 = QtWidgets.QHBoxLayout()
-        hb0.addWidget(self.view)
+        hb0.addWidget(self.memo_list_view)
         hb0.addLayout(vb)
 
-        if hasattr(self.view.header(), 'setResizeMode'):
-            # PySide
-            self.view.header().setResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
-            self.view.header().setResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
-            self.view.header().setResizeMode(2, QtWidgets.QHeaderView.ResizeToContents)
+        _hed = self.memo_list_view.header()
+        _rtc = QtWidgets.QHeaderView.ResizeToContents
+        if hasattr(_hed, 'setResizeMode'):
+            [_hed.setResizeMode(i, _rtc) for i in range(3)]  # PySide
         else:
-            # PySide2
-            self.view.header().setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
-            self.view.header().setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
-            self.view.header().setSectionResizeMode(2, QtWidgets.QHeaderView.ResizeToContents)
+            [_hed.setSectionResizeMode(i, _rtc) for i in range(3)]  # PySide2
 
-        self.set_item()
-        self.view.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.set_list_item()
+        self.memo_list_view.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         #self.view.customContextMenuRequested.connect(self._context)
-        self.view.setAlternatingRowColors(True)
+        self.memo_list_view.setAlternatingRowColors(True)
 
-        self.setLayout(hb0)
 
-    def set_item(self):
+        # ダイアログのOK/キャンセルボタンを用意
+        btns = QtWidgets.QDialogButtonBox(
+            QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel,
+            QtCore.Qt.Horizontal, self)
+        btns.accepted.connect(self.accept)
+        btns.rejected.connect(self.reject)
+
+        vb0 = QtWidgets.QVBoxLayout()
+        vb0.addLayout(hb0)
+        vb0.addWidget(btns)
+        self.setLayout(vb0)
+
+    def _memo_list_changed(self, selected, deselected):
+        if len(deselected) != 0:
+            _deselected_idx = deselected[0].indexes()[0].row()
+            _selected_idx = selected[0].indexes()[0].row()
+            #前回選択していたメモの内容を反映
+            _comment = self.get_comment()
+            _fr = self.get_fr()
+            _color = self.get_color()
+            _dict = {'comment': _comment, 'fr': _fr, 'bg_color': _color}
+            self.draw_data[_deselected_idx] = _dict
+            self.set_list_item()
+            #選択状態を復元
+            self.memo_list_view.setCurrentIndex(self.memo_list_model.index(_selected_idx, 0))
+        self._apply_select_memo_data()
+
+    def _apply_select_memo_data(self):
+
+        _list_idx = self.memo_list_view.currentIndex()
+        _d = self.draw_data[_list_idx.row()]
+
+        self.le.setPlainText(_d['comment'])
+        self.fr_start.setValue(_d['fr'][0])
+        self.fr_end.setValue(_d['fr'][1])
+        self.color = _d['bg_color']
+        self._set_button_color()
+
+    def set_list_item(self):
         index = 0
         for _d in self.draw_data:
-            self.model.setItem(index, 0, QtGui.QStandardItem(str(_d['fr'][0]) + u'～' + str(_d['fr'][1]) + '  '))
-            self.model.setItem(index, 1, QtGui.QStandardItem(_d['bg_color']))
-            self.model.setItem(index, 2, QtGui.QStandardItem(_d['comment']))
+            self.memo_list_model.setItem(index, 0, QtGui.QStandardItem(str(_d['fr'][0]) + u'～' + str(_d['fr'][1]) + '  '))
+            self.memo_list_model.setItem(index, 1, QtGui.QStandardItem(_d['bg_color']))
+            self.memo_list_model.setItem(index, 2, QtGui.QStandardItem(_d['comment']))
             index += 1
 
     @staticmethod
